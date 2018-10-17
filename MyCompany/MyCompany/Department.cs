3e2c8b01434059170360de44e33a48ee11a97514
NataliaMorozova <natali442005@mail.ru>
2018-10-17 23:44:26 +0300

    Дз к С# Уровень 2 Урок 5

---
+++ MyCompany/MyCompany/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany
+{
+    class Department
+    {
+
+        public string department;
+
+        public Department(string department)
+        {
+            this.department = department;
+        }
+
+        public override string ToString()
+        {
+            return department;
+        }
+    }
+}
